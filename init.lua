f51cbf0b4d013e34643ccc74e5aa28c171cbe843
Naufil (Z-xus) <naufilasar.dev@gmail.com>
2024-12-25 12:33:57 +0530

    feat(nvim-ts): add incremental selection and textobject keybinds

--- init.lua
+++ init.lua
@@ -773,12 +773,12 @@ require("lazy").setup({
 			indent = { enable = true, disable = { "ruby" } },
 
 			incremental_selection = {
-				enable = false,
+				enable = true,
 				keymaps = {
-					init_selection = false, -- set to `false` to disable one of the mappings
-					node_incremental = false,
-					scope_incremental = false,
-					node_decremental = false,
+					init_selection = "<leader>ii", -- Start incremental selection
+					node_incremental = "<leader>in", -- Increment to the next node
+					scope_incremental = "<leader>is", -- Increment to the next scope
+					node_decremental = "<leader>id", -- Decrement to the previous node
 				},
 			},
 			-- textobjects
@@ -821,7 +821,7 @@ require("lazy").setup({
 					set_jumps = true, -- whether to set jumps in the jumplist
 					goto_next_start = {
 						["]m"] = "@function.outer",
-						["]]"] = { query = "@class.outer", desc = "Next class start" },
+						-- ["]]"] = { query = "@class.outer", desc = "Next class start" },
 						--
 						-- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
 						["]l"] = "@loop.*",
@@ -829,21 +829,22 @@ require("lazy").setup({
 						--
 						-- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
 						-- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-						["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+						-- ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
 						["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
 					},
-					goto_next_end = {
-						["]M"] = "@function.outer",
-						["]["] = "@class.outer",
-					},
+					-- goto_next_end = {
+					-- 	["]M"] = "@function.outer",
+					-- 	["]["] = "@class.outer",
+					-- },
 					goto_previous_start = {
 						["[m"] = "@function.outer",
-						["[["] = "@class.outer",
+						["[l"] = "@loop.*",
+						-- ["[["] = "@class.outer",
 					},
-					goto_previous_end = {
-						["[M"] = "@function.outer",
-						["[]"] = "@class.outer",
-					},
+					-- goto_previous_end = {
+					-- 	["[M"] = "@function.outer",
+					-- 	["[]"] = "@class.outer",
+					-- },
 				},
 			},
 		},
