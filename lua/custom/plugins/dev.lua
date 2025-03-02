8b8ea3dbc42b924fa83d8ac7d9d2af9e6f5cfb79
Naufil (Z-xus) <naufilasar.dev@gmail.com>
2025-03-02 16:57:52 +0530

    change shortcut to easier one

--- lua/custom/plugins/dev.lua
+++ lua/custom/plugins/dev.lua
@@ -9,10 +9,13 @@ return {
 	},
 	{
 		-- dir = "~/Personal/float.nvim",
+		-- TODO: 1. Infinite output hangs the buffer
+		-- TODO: 2. Some vim like navigation for selection and copy
+		-- TODO: 3. Transfer to tab or something to make bigger or allow resize
 		"Z-xus/float.nvim",
 		lazy = false,
 		opts = {
-			keybind = "<leader>tt",
+			keybind = "<leader>t",
 		},
 	},
 }
